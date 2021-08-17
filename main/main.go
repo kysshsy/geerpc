25fab76de7176006f19f677774ced44fbea7e8f7
kysshsy <kysshsy@gmail.com>
2021-08-17 10:55:29 +0800

    feature(test): 增加并发client测试

--- main/main.go
+++ main/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"geerpc"
-	"geerpc/codec"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -31,28 +30,25 @@ func main() {
 
 	time.Sleep(time.Second)
 
-	conn, err := net.Dial("tcp", <-addr)
-
+	client, err := geerpc.Dail("tcp", <-addr)
 	if err != nil {
-		panic("connect eroor")
+		log.Panic("rpc client init error")
 	}
 
-	time.Sleep(time.Second)
-
-	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
-
-	cc := codec.NewGobCodec(conn)
-
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 
 	}
+	wg.Wait()
 }
