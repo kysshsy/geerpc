0edc05300e25b8fa3b2e6c3c78727b5c95b80e6f
kysshsy <kysshsy@gmail.com>
2021-09-14 21:45:27 +0800

    feat: rpc测试程序

--- main/main.go
+++ main/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"geerpc"
 	"log"
 	"net"
@@ -9,46 +8,51 @@ import (
 	"time"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
-	lis, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Printf("main: startServer error\n")
-		panic("startServer")
+	var foo Foo
+	if err := geerpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
 	}
-	log.Println("start rpc server on", lis.Addr())
-
-	server := geerpc.NewServer()
-	addr <- lis.Addr().String()
-
-	server.Accept(lis)
-
+	// pick a free port
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	geerpc.Accept(l)
 }
 
 func main() {
+	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
+	client, _ := geerpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-
-	client, err := geerpc.Dail("tcp", <-addr)
-	if err != nil {
-		log.Panic("rpc client init error")
-	}
-
+	// send request & receive response
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			args := fmt.Sprintf("geerpc req %d", i)
-			var reply string
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
 			if err := client.Call("Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
-			log.Println("reply:", reply)
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
-
 	}
 	wg.Wait()
 }
